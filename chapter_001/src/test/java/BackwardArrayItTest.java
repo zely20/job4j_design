7ed4557c1d9b63c5886f58f307127f2ad558d1a9
zely20@mail.ru <Civik1984>
2020-07-19 09:17:44 +0300

    Что такое итератор.[4951#329133]

---
+++ chapter_001/src/test/java/BackwardArrayItTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BackwardArrayItTest {
+
+
+    @Test
+    public void whenMultiCallhasNextThenTrue() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertThat(it.hasNext(), is(true));
+        assertThat(it.hasNext(), is(true));
+    }
+
+    @Test
+    public void whenReadSequence() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[] {1, 2, 3}
+        );
+        assertThat(it.next(), is(3));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(1));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenNextFromEmpty() {
+        BackwardArrayIt it = new BackwardArrayIt(
+                new int[] {}
+        );
+        it.next();
+    }
+}
