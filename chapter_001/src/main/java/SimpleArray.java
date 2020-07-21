9618dac12bfc6e74b4200dabc1e964bd01a9c9dd
zely20@mail.ru <Civik1984>
2020-07-21 21:21:00 +0300

    5.2.1. Реализовать SimpleArray<T>[156#329145]

---
+++ chapter_001/src/main/java/SimpleArray.java
@@ -0,0 +1,57 @@
+import java.util.Iterator;
+
+public class SimpleArray<T> {
+
+    private Object[] array;
+    private int pointer = 0;
+
+    public SimpleArray(int size) {
+        this.array = new Object[size];
+    }
+
+    public void add(T value) {
+        if (pointer < array.length) {
+            array[pointer++] = value;
+        }
+    }
+
+    public void set(int index, T model) {
+        if (index < array.length) {
+            array[index] = model;
+        }
+    }
+
+    public void remove(int index) {
+        if (index < array.length) {
+            for (int i = index; i < pointer; i++) {
+                if (i == pointer-1) {
+                    array[i] = null;
+                } else {
+                    array[i] = array[i + 1];
+                }
+            }
+        }
+    }
+
+    public T get(int index) {
+        return (T) array[index];
+    }
+
+    public Iterator<T> iterator() {
+        return (Iterator<T>) new SimpleArrayIt();
+    }
+
+    private class SimpleArrayIt implements Iterator<T> {
+
+        private int cursor = 0;
+        @Override
+        public boolean hasNext() {
+            return cursor < array.length;
+        }
+
+        @Override
+        public T next() {
+            return (T) array[cursor++];
+        }
+    }
+}
