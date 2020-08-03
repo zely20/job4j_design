918086f1424e04dd430fa5097e76edb7b408cfee
zely20@mail.ru <Civik1984>
2020-08-03 20:22:34 +0300

    SimpleSet refactoring

--- chapter_001/src/main/java/ru/job4j/collection/SimpleSet.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleSet.java
@@ -1,18 +1,26 @@
 package ru.job4j.collection;
 
 import java.util.Iterator;
+import java.util.Objects;
 
 public class SimpleSet<E> implements Iterable<E> {
-    SimpleArray<E> set = new SimpleArray<E>();
+    private SimpleArray<E> set = new SimpleArray<E>();
 
     public void add(E e) {
+        if (!contains(e)) {
+            set.add(e);
+        }
+    }
+
+    public boolean contains(E e) {
+        boolean result = false;
         for (int i = 0; i < set.size(); i++) {
-            if (set.get(i).equals(e)) {
-                return;
+            if (Objects.equals(set.get(i), e)) {
+                result = true;
+                break;
             }
         }
-        ;
-        set.add(e);
+        return result;
     }
 
     @Override
