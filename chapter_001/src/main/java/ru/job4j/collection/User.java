4427b8867dbc945f0558ee3c5486772b053aa113
zely20@mail.ru <Civik1984>
2020-08-03 20:24:49 +0300

    User refactoring

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -3,9 +3,9 @@ package ru.job4j.collection;
 import java.util.*;
 
 public class User {
-    String name;
-    int children;
-    Calendar birthday;
+    private String name;
+    private int children;
+    private Calendar birthday;
 
     public User(){
 
