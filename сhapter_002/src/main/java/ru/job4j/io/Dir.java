fb16bbd605bfd45694a65967aadf134c0ff1c145
Aliaksandr Zelianko <zely20@mail.ru>
2020-09-01 15:20:22 +0300

    4.0. File[252491#329175]

---
+++ сhapter_002/src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\project");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            System.out.println("Name file: " + subfile.getName() + " size: " + subfile.getTotalSpace());
+        }
+    }
+}
