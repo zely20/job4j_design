e20e2a315fb4737f9f552667e56267ffff03f0b0
zely20@mail.ru <Civik1984>
2020-09-02 21:53:25 +0300

    SearchFiles

---
+++ сhapter_002/src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,46 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles implements FileVisitor<Path> {
+
+    private Predicate<Path> pr;
+    List<Path> paths;
+
+    public SearchFiles(Predicate<Path> pr) {
+        this.pr = pr;
+        paths = new ArrayList<>();
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return null;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println(file.toAbsolutePath());
+        if(pr.test(file.toAbsolutePath())){
+            paths.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return null;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return null;
+    }
+}
