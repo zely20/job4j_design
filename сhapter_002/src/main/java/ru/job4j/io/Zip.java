b325ab5bc4110a3774b8901c9f3a01e09fb1707e
Aliaksandr Zelianko <zely20@mail.ru>
2020-09-08 16:56:14 +0300

    5.2. Архивировать проект[861#329180]

--- сhapter_002/src/main/java/ru/job4j/io/Zip.java
+++ сhapter_002/src/main/java/ru/job4j/io/Zip.java
@@ -1,15 +1,27 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.Arrays;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
     public void packFiles(List<File> sources, File target) {
-
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File file : sources) {
+                zip.putNextEntry(new ZipEntry(file.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void packSingleFile(File source, File target) {
@@ -23,14 +35,17 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ArgZip zip1 = new ArgZip(args);
-        System.out.println(zip1.directory());
-        System.out.println(zip1.output());
-        System.out.println(zip1.exclude());
-       /* new Zip().packSingleFile(
-                new File("pom.xml"),
-                new File("pom.zip")
-        );*/
+        SearchFiles searcher = new SearchFiles(p -> !p.toFile().getName().endsWith(zip1.exclude()));
+        Files.walkFileTree(Paths.get(zip1.directory()), searcher);
+        List<File> files = searcher.paths
+                .stream()
+                .map(path -> path.toFile())
+                .collect(Collectors.toList());
+        new Zip().packFiles(files, new File(zip1.output()));
+        for (Path st : searcher.paths){
+            System.out.println(st.toFile().getAbsolutePath());
+        }
     }
 }
