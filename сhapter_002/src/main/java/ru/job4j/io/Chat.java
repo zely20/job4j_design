a9eb7e5359c5084a67faee46dde5824733e180b9
Aliaksandr Zelianko <zely20@mail.ru>
2020-09-09 16:29:17 +0300

    6. Кодировка.[862#329181]

--- сhapter_002/src/main/java/ru/job4j/io/Chat.java
+++ сhapter_002/src/main/java/ru/job4j/io/Chat.java
@@ -1,10 +1,7 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Chat {
     Map<Integer, String> randomText = new HashMap<>();
@@ -20,20 +17,45 @@ public class Chat {
         }
     }
 
-    public static void main(String[] args) {
-        boolean isWork = true;
-        String inText;
-        Chat chat = new Chat();
-        chat.loadText();
-
-        while (isWork){
-            int a = (int) ( Math.random() * 4 );
-            try (Scanner in = new Scanner(System.in)){
-                inText = in.nextLine();
-            } catch (Exception e) {
+    public void save(String log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file, true)
+                ))) {
+            out.write(log + System.lineSeparator());
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    public static void main(String[] args) {
+        boolean isWork = true;
+        boolean isStop = false;
+        String inText = null;
+        Chat chat = new Chat();
+        chat.loadText();
+        System.out.println("С вами говорит Бот");
+        while (isWork) {
+            int a = (int) ( Math.random() * 4 );
+            Scanner in = new Scanner(System.in);
+                inText = in.nextLine();
+            if(inText.equals("стоп")) {
+                System.out.println("Чат остановлен!!!");
+                chat.save(inText, "logchat.txt");
+                isStop = true;
+            } else  if(inText.equals("закончить")){
+                chat.save(inText, "logchat.txt");
+                System.out.println("Чат завершен!");
+                isWork = false;
+            }
+            if(!isStop && isWork) {
+                System.out.println(chat.randomText.get(a));
+            }
+            if(inText.equals("продолжить")){
+                System.out.println("Продолжаем!!!");
+                chat.save(inText, "logchat.txt");
+                isStop = false;
+            }
         }
     }
 }
