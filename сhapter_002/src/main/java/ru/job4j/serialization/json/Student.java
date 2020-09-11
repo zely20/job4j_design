6a56e3d9f76262a18d187891d1baed7a08542ecb
Aliaksandr Zelianko <zely20@mail.ru>
2020-09-11 15:44:43 +0300

    5. Преобразование JSON в POJO. JsonObject[315064#329188]

--- сhapter_002/src/main/java/ru/job4j/serialization/json/Student.java
+++ сhapter_002/src/main/java/ru/job4j/serialization/json/Student.java
@@ -2,6 +2,7 @@ package ru.job4j.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONObject;
 
 public class Student {
     private int age;
@@ -18,6 +19,26 @@ public class Student {
         this.course = course;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public boolean isStudy() {
+        return isStudy;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
     @Override
     public String toString() {
         return "Student{" +
@@ -31,7 +52,16 @@ public class Student {
 
     public static void main(String[] args) {
         final Student student = new Student(25,"Zelenko","Alex", true,1);
-        final Gson gson = new GsonBuilder().create();
+        JSONObject jsonStudent = new JSONObject();
+        jsonStudent.put("age", student.getAge());
+        jsonStudent.put("firstName", student.getFirstName());
+        jsonStudent.put("secondName", student.getSecondName());
+        jsonStudent.put("isStudy", student.isStudy);
+        jsonStudent.put("course", student.getCourse());
+        System.out.println(jsonStudent);
+
+        System.out.println(new JSONObject(student).toString());
+        /*final Gson gson = new GsonBuilder().create();
         System.out.println(gson.toJson(student));
 
         final String jSon = "{" +
@@ -41,6 +71,6 @@ public class Student {
                 "\"isStudy\":true," +
                 "\"course\":1}";
         final Student student2 = gson.fromJson(jSon,Student.class);
-        System.out.println(student2);
+        System.out.println(student2);*/
     }
 }
