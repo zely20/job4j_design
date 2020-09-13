7bb53ef4b5e7bea1a521e3c3970037d29678db43
zely20@mail.ru <Civik1984>
2020-09-13 10:40:41 +0300

    ReapetFile refactoring

--- сhapter_002/src/main/java/ru/job4j/io/RepeatFile.java
+++ сhapter_002/src/main/java/ru/job4j/io/RepeatFile.java
@@ -5,29 +5,39 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class RepeatFile implements FileVisitor<Path> {
 
-    HashMap<Long, Path> set = new HashMap<>();
-    Set<Path> set2 = new HashSet<>();
-    List<Path> list = new LinkedList<>();
+    private Map<Long, List<Path>> map = new HashMap<>();
+    private List<Path> list = new LinkedList<>();
 
     @Override
-    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
         return FileVisitResult.CONTINUE;
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (set.containsKey(file.toFile().length())) {
-            String temp1 = set.get(file.toFile().length()).toFile().getName();
-            String temp2 = file.toFile().getName();
-            if (temp1.equals(temp2)) {
-                list.add(file);
+        long id = file.toFile().length();
+        if (map.containsKey(id)) {
+            List<Path> tempFromMap = map.get(id);
+            for (Path path : tempFromMap) {
+                if (path.toFile().getName().equals(file.toFile().getName())) {
+                    list.add(file);
+                } else {
+                    List<Path> temp = map.get(id);
+                    temp.add(file);
+                    map.put(id, temp);
+                }
             }
         } else {
-            set.put(file.toFile().length(), file);
+            List<Path> inMap = new LinkedList<>();
+            inMap.add(file);
+            map.put(file.toFile().length(), inMap);
         }
         return FileVisitResult.CONTINUE;
     }
