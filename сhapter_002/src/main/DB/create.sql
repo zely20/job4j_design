ab59f036e0112112660040f969fae38a5a9b0f7f
Aliaksandr Zelianko <zely20@mail.ru>
2020-09-25 15:43:38 +0300

    Фильтры[1732#329107]

--- сhapter_002/src/main/DB/create.sql
+++ сhapter_002/src/main/DB/create.sql
@@ -7,7 +7,7 @@ INSERT INTO type (name) VALUES ('МЯСО');
 INSERT INTO type (name) VALUES ('МОЛОКО');
 INSERT INTO type (name) VALUES ('МОРОЖЕННОЕ');
 
-INSERT INTO product (name, type_id, expired_date, price) VALUES ('Camamber', 1, '2020-10-29', 100);
+INSERT INTO product (name, type_id, expired_date, price) VALUES ('Camamber2', 1, '2020-10-29', 100);
 INSERT INTO product (name, type_id, expired_date, price) VALUES ('Blue Chees', 1, '2020-10-29', 90);
 INSERT INTO product (name, type_id, expired_date, price) VALUES ('Milk', 3, '2020-11-20', 30);
 INSERT INTO product (name, type_id, expired_date, price) VALUES ('Black Milk', 3, '2020-12-29', 20);
@@ -15,6 +15,7 @@ INSERT INTO product (name, type_id, expired_date, price) VALUES ('Milk IceCream'
 INSERT INTO product (name, type_id, expired_date, price) VALUES ('Chokolate IceCream', 4, '2020-12-29', 20);
 
 select * from product;
+
 --Написать запрос получение всех продуктов с типом "СЫР"
 SELECT p.id AS id, p.name AS name, t.name AS name
 FROM product AS p
@@ -22,17 +23,29 @@ LEFT JOIN
 type AS t
 ON
 p.type_id = t.id
-WHERE p.type_id = 1;
+WHERE t.name = 'СЫР';
+
+
 --Написать запрос получения всех продуктов, у кого в имени есть слово "мороженное"
 SELECT * FROM product WHERE product.name LIKE '%IceCream';
+
 --Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
-SELECT * FROM product WHERE product.expired_date < '2020-11-01';
+SELECT * FROM product WHERE  product.expired_date - current_date < 31;
+
+
 --Написать запрос, который выводит самый дорогой продукт.
 SELECT * FROM product
 WHERE product.price = (SELECT MAX(price) FROM product);
 --Написать запрос, который выводит количество всех продуктов определенного типа.
-SELECT type_id, COUNT(type_id) FROM product
-GROUP BY type_id;
+SELECT p.type_id, COUNT(p.type_id)  FROM product p
+GROUP BY p.type_id;
+
+SELECT t.name, COUNT(t.name) from product p
+left join type t
+on p.type_id=t.id
+group by t.name
+;
+
 --Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
 --НЕ РАБОТАЕТ НЕ ПОЙМУ
 SELECT p.id AS id, p.name AS name, t.name AS name
@@ -41,9 +54,16 @@ LEFT JOIN
 type AS t
 ON
 p.type_id = t.id
-WHERE (p.type_id = 1 AND p.type_id = 3);
+WHERE (t.name = 'СЫР' OR t.name = 'МОЛОКО');
 --Вывести все продукты и их тип.
 select p.name, p.expired_date, p.price, t.name from product AS p, type AS t
 where t.id=p.type_id;
 
 SELECT * FROM product ;
+
+SELECT t.name, COUNT(t.name)  as c from product p
+left join type t
+on p.type_id=t.id
+group by t.name
+having COUNT(t.name) < 10
+;
