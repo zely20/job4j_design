e7ab6e9cdbc80fce43013c4c27dcc2cb16f03ffe
zely20@mail.ru <Civik1984>
2020-10-23 22:16:33 +0300

    4. Зона видимости в лямбда и исключения[70609#239970]

---
+++ chapter_001/src/main/java/ScopeInside.java
@@ -0,0 +1,21 @@
+import java.util.function.Supplier;
+
+public class ScopeInside {
+    public static void main(String[] args) {
+        int[] number = {1, 2, 3};
+        int total = 0;
+        for (int i = 0; i < number.length; i++) {
+            int num = number[i];
+            total +=  add(
+                    () -> {
+                        return num;
+                    }
+            );
+        }
+        System.out.println(total);
+    }
+
+    private static Integer add(Supplier<Integer> calc) {
+        return calc.get();
+    }
+}
