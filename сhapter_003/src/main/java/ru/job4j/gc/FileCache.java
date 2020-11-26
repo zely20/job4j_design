6bbc15611530e739fc735dcdb38d6f00db987b31
Aliaksandr Zelianko <zely20@mail.ru>
2020-11-26 15:00:56 +0300

    refactoring

--- сhapter_003/src/main/java/ru/job4j/gc/FileCache.java
+++ сhapter_003/src/main/java/ru/job4j/gc/FileCache.java
@@ -11,23 +11,34 @@ public class FileCache implements Cache<String, String>{
     private HashMap<String, SoftReference<String>> cache = new HashMap<>();
 
     public String get(String key) throws IOException {
+        String data = null;
         if (cache.containsKey(key)) {
             if (cache.get(key) != null) {
                 return cache.get(key).get();
+            } else {
+                data = readFile(key);
+                put(key, data);
+                return cache.get(key).get();
             }
         } else {
-            StringBuilder builder = new StringBuilder();
-            BufferedReader read = new BufferedReader(new FileReader(key));
-            while (read.ready()) {
-                builder.append(read.readLine());
-            }
-            put(key, builder.toString());
+            data = readFile(key);
+            put(key, data);
         }
         return cache.get(key).get();
     }
 
     public void put(String key, String value) {
+
         SoftReference<String> softReference = new SoftReference<String>(value);
         this.cache.put(key, softReference);
+    }
+
+    public String readFile(String filename) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        BufferedReader read = new BufferedReader(new FileReader(filename));
+        while (read.ready()) {
+            builder.append(read.readLine());
+        }
+        return builder.toString();
     }
 }
