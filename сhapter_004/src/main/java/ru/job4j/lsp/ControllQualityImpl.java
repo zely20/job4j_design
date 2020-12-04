8c5657f6e47eb740dbc49893e3f0c6c3845358c2
Aliaksandr Zelianko <zely20@mail.ru>
2020-12-04 13:16:06 +0300

    refactoring

--- сhapter_004/src/main/java/ru/job4j/lsp/ControllQualityImpl.java
+++ сhapter_004/src/main/java/ru/job4j/lsp/ControllQualityImpl.java
@@ -1,23 +1,17 @@
 package ru.job4j.lsp;
 
-import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
-public class ControllQualityImpl {
+public class ControllQualityImpl implements ControllQualityStrategy{
 
-    /*@Override
-    public void distribute(Food food) {
-        long remainderDate = food.expaireDate.getTime() - new Date().getTime();
-        long commonExpaireDate = food.expaireDate.getTime() - food.createDate.getTime();
-        if (remainderDate <= 0) {
-            System.out.println("Срок годности вышел");
+    private Set<Store> mapStore = new HashSet<>();
+
+    @Override
+    public void addStore(Store store) {
+        if(store != null){
+            mapStore.add(store);
         }
-        long percent = remainderDate*100/commonExpaireDate;
-        if(percent < 25) {
-            warehouse.addFood(food);
-        } else if(percent < 75 && percent > 25) {
-            shop.addFood(food);
-        } else {
-            trash.addFood(food);
-        }
-    }*/
+
+    }
 }
--- сhapter_004/src/main/java/ru/job4j/lsp/ControllQualityStrategy.java
+++ сhapter_004/src/main/java/ru/job4j/lsp/ControllQualityStrategy.java
@@ -2,5 +2,5 @@ package ru.job4j.lsp;
 
 public interface ControllQualityStrategy {
 
-    public void distribute(Food food);
+    public void addStore(Store store);
 }
